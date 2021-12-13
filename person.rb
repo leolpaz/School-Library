d175388b59a798b81f8bc0cd308a18ca38a67b4f
leolpaz <leonardolpaz95@gmail.com>
2021-12-13 12:19:59 -0300

    Add person class

---
+++ person.rb
@@ -0,0 +1,20 @@
+class Person
+  attr_accessor :age, :name
+  attr_reader :id
+  def initialize(name, age, parent_permission = false)
+    @id = rand(1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    return true if is_of_age?() || @parent_permission
+    false
+  end
+
+  private
+  def is_of_age?
+    @age >= 18
+  end
+end
